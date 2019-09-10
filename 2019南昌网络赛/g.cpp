b994b54e610dd843d08e2bce58e2e82b6d0c2c6f
Userwhite <49226823+Userwhite@users.noreply.github.com>
2019-09-10 23:38:19 +0800

    Create g.cpp

---
+++ 2019南昌网络赛/g.cpp
@@ -0,0 +1,27 @@
+#include <set>
+#include <map>
+#include <queue>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <limits.h>
+#include <cstdio>
+#include <bitset>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#define FOR(i,l,r) for(int i=l;i<=r;i++)
+#define inf 0x3f3f3f3f
+using namespace std;
+
+typedef long long ll;
+const int maxn=1e3+500;
+
+int main(){
+    int T;cin>>T;
+    while(T--){
+        int n;cin>>n;
+        if(n==1)puts("18000");
+        else puts("0");
+    }
+}
